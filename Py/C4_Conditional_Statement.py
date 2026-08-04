61a74bdff2923b5e68536dd5a2e990738fd9ed4e
Sapna Vijayakumar <vsapnaa2002@gmail.com>
2026-08-04 12:18:38 +0800

    Update Chapter 4 PY

---
+++ Py/C4_Conditional_Statement.py
@@ -0,0 +1,17 @@
+#Concept Conditional Statement
+
+#if-else: 2 conditions
+#if-elif-else: more than 2 conditions
+
+#Example A
+
+age = int(input("Enter you're age: "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+
+
+
