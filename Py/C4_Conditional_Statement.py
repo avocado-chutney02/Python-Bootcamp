65dbd6e97c76639ad7e9a09dad63afb9189feeeb
Sapna Vijayakumar <vsapnaa2002@gmail.com>
2026-08-04 12:41:05 +0800

    Update Chapter 4 py

--- Py/C4_Conditional_Statement.py
+++ Py/C4_Conditional_Statement.py
@@ -1,9 +1,6 @@
 #Concept Conditional Statement
 
-#if-else: 2 conditions
-#if-elif-else: more than 2 conditions
-
-#Example A
+#Example A: if-else: 2 conditions
 
 age = int(input("Enter you're age: "))
 
@@ -11,6 +8,31 @@ if age >= 18:
     print("You are an adult.")
 else:
     print("You are a minor.")
+
+#Example B: if-elif-else: more than 2 conditions
+
+score = int(input("Enter your score: "))
+
+if score >= 90:
+      grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"Your grade is: {grade}")
+
+#Example C: and: both conditions must be True
+
+user_age = 25 
+has_license = True
+
+
+
 
 
 
